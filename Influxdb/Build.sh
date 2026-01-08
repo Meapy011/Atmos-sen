e762ae5bcea7a81e2e9cf879511c0af98aec98b9
x86 <meap09121@gmail.com>
2026-01-08 15:33:45 -0800

    Updated Build.sh

--- Influxdb/Build.sh
+++ Influxdb/Build.sh
@@ -2,7 +2,7 @@
 
 #Checking for Updates and installing Essential tools
 sudo apt update -y
-sudo apt install build-essential -y
+sudo apt install build-essential libcurl4-openssl-dev -y
 
 #Running Makefile
 make clean
